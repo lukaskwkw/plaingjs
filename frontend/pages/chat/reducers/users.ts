d6fcf7458b9cc7a6b6350e8501b9172ca4d09220
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-07-21 20:20:46 +0200

    Add todo

--- frontend/pages/chat/reducers/users.ts
+++ frontend/pages/chat/reducers/users.ts
@@ -2,12 +2,13 @@ import TypeKeys, { UserActionTypes } from "../ActionTypes";
 import { User } from "../model";
 
 interface UsersReducer {
-  (state: User[], action: User & UserActionTypes & { users: User[] }): User[];
+  (state: User[], action: User & { users: User[] } & UserActionTypes): User[];
 }
 
 const users: UsersReducer = (state = [], action) => {
   switch (action.type) {
     case TypeKeys.ADD_USER:
+      //TODO: Check if interfaces are correctly written as before it was possible to distinct action properties by its type
       return [...state, { name: action.name, id: action.id }];
     case TypeKeys.USERS_LIST:
       return action.users;
