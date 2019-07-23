4cc2603116b136828328504d55148a8a230a3cfb
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-07-23 13:15:35 +0200

    Setup global runtime env for all microservices

---
+++ chat-webserver/index.ts
@@ -0,0 +1,16 @@
+// import WebSocket from "ws";
+
+// import { StoreState } from "../frontend/store";
+// import TypeKeys from "../frontend/pages/chat/ActionTypes";
+
+console.info("test");
+
+// const wss = new WebSocket.Server({ port: 9124 });
+
+// wss.on("connection", ws => {
+//   ws.on("message", message => {
+//     console.log("received: %s", message);
+//   });
+
+//   ws.send("something");
+// });
