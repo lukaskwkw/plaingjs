7dff14b3c11c872df3ef4fef0248b4b1b87b3627
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-07-23 15:33:50 +0200

    Add debug option for chat webservice

--- chat-webserver/index.ts
+++ chat-webserver/index.ts
@@ -1,16 +1,17 @@
-// import WebSocket from "ws";
+import * as WebSocket from "ws";
 
-// import { StoreState } from "../frontend/store";
-// import TypeKeys from "../frontend/pages/chat/ActionTypes";
+import { StoreState } from "../frontend/store";
+import TypeKeys from "../frontend/pages/chat/ActionTypes";
 
-console.info("test");
+// const WebSocket = require("ws");
 
-// const wss = new WebSocket.Server({ port: 9124 });
+// const wss = new WebSocket("ws://localhost:9124/");
+const wss = new WebSocket.Server({ port: 9124 });
 
-// wss.on("connection", ws => {
-//   ws.on("message", message => {
-//     console.log("received: %s", message);
-//   });
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    console.log("received: %s", message);
+  });
 
-//   ws.send("something");
-// });
+  ws.send("something");
+});
